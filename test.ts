3d14be1a7b80afa73dbc2634261b73d46d38e52a
Petr Mácha <91247806+PetrMacha021@users.noreply.github.com>
2023-06-20 10:15:24 +0200

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,8 @@
-// tests go here; this will not be compiled when this package is used as an extension.
+let motory = motorsynchronization.create(
+PCAmotor.Motors.M0,  // Left motor
+PCAmotor.Motors.M1,  // Right motor
+DigitalPin.P0,       // Sensor for left motor
+DigitalPin.P1,       // Sensor for right motor
+20                   // Number of holes in the encoder
+)
+motory.Run(-20, 20)  // Start the motors
